7728f501645f9a2665c1bf11830662c848dd2743
TristanDs <d.s.tristan@outlook.fr>
2021-12-03 22:51:44 +0100

    un peu de css

--- vues/v_affichermedoc.php
+++ vues/v_affichermedoc.php
@@ -8,16 +8,16 @@
         <div class="feature-work container my-4">
             <div class="row d-flex d-flex align-items-center">
                 <div class="col-lg-5">
-                    <h1 class="feature-work-heading h2 py-3 semi-bold-600">Informations du médicament :<br/><span style="text-decoration: underline;"><?php echo $carac['MED_NOMCOMMERCIAL']; ?></span></h1>
+                    <h1 class="feature-work-heading h2 py-3 semi-bold-600">Informations du médicament :<br/><span class="carac"><?php echo $carac['MED_NOMCOMMERCIAL']; ?></span></h1>
                     <a class="col" data-type="image" data-fslightbox="gallery" href="assets/img/medicament.jpg">
                         <img class="img-fluid" src="assets/img/medoc.jpeg">
                     </a>
                 </div>
-                <div class="col-lg-4 offset-lg-1 align-left">
+                <div class="col-lg-4 offset-lg-1 align-left affich">
                     <div class="row">
-                         <?php echo '<p><span style="text-decoration: underline;">Dépot légal</span> : '.$carac[0].'</p><br/><br/>
-                            <p><span style="text-decoration: underline;">Nom commercial</span> : '.$carac[1].'</p><br/><br/><p><span style="text-decoration: underline;">Composition</span> : '.$carac[2].'</p><br/><br/>
-                            <p><span style="text-decoration: underline;">Effets</span> : '.$carac[3].'</p><br/><br/><p><span style="text-decoration: underline;">Contre indication</span> : '.$carac[4] ; ?></p>
+                         <?php echo '<p><span class="carac">Dépot légal</span> : '.$carac[0].'</p><br/><br/>
+                            <p><span class="carac">Nom commercial</span> : '.$carac[1].'</p><br/><br/><p><span class="carac">Composition</span> : '.$carac[2].'</p><br/><br/>
+                            <p><span class="carac">Effets</span> : '.$carac[3].'</p><br/><br/><p><span class="carac">Contre indication</span> : '.$carac[4] ; ?></p>
                     </div>
                     <div class="row">
                        <input class="btn btn-info text-light" type="button" onclick="window.location.href='index.php?uc=medicaments&action=formulairemedoc';" value="Retour">
--- vues/v_afficherpraticien.php
+++ vues/v_afficherpraticien.php
@@ -7,21 +7,21 @@
         <div class="feature-work container my-4">
             <div class="row d-flex d-flex align-items-center">
                 <div class="col-lg-5">
-                    <h1 class="feature-work-heading h2 py-3 semi-bold-600">Informations du praticien :<br/><span style="text-decoration: underline;"><?php echo $carac['0'].' '.$carac['1']; ?></span></h1>
+                    <h1 class="feature-work-heading h2 py-3 semi-bold-600">Informations du praticien :<br/><span class="carac"><?php echo $carac['0'].' '.$carac['1']; ?></span></h1>
                     <a class="col" data-type="image" data-fslightbox="gallery" href="assets/img/medicament.jpg">
                         <img class="img-fluid" src="assets/img/medoc.jpeg">
                     </a>
                 </div>
-                <div class="col-lg-4 offset-lg-1 align-left">
+                <div class="col-lg-4 offset-lg-1 align-left affich">
                     <div class="row">
                          <?php 
                             if(empty($carac[6])){
                                 $carac[6]='Non définie';
                             }                            
-                            echo '<p><span style="text-decoration: underline;">Nom</span> : '.$carac[0].'</p><br/><br/>
-                            <p><span style="text-decoration: underline;">Prénom</span> : '.$carac[1].'</p><br/><br/><p><span style="text-decoration: underline;">Adresse</span> : '.$carac[2].'</p><br/><br/>
-                            <p><span style="text-decoration: underline;">Code Postal</span> : '.$carac[3].'</p><br/><br/><p><span style="text-decoration: underline;">Ville</span> : '.$carac[4].'</p><br/><br/>
-                            <p><span style="text-decoration: underline;">Notoriété</span> : '.$carac[5].'</p><br/><br/><p><span style="text-decoration: underline;">Confiance</span> : '.$carac[6] ; ?></p>
+                            echo '<p><span class="carac">Nom</span> : '.$carac[0].'</p><br/><br/>
+                            <p><span class="carac">Prénom</span> : '.$carac[1].'</p><br/><br/><p><span class="carac">Adresse</span> : '.$carac[2].'</p><br/><br/>
+                            <p><span class="carac">Code Postal</span> : '.$carac[3].'</p><br/><br/><p><span class="carac">Ville</span> : '.$carac[4].'</p><br/><br/>
+                            <p><span class="carac">Notoriété</span> : '.$carac[5].'</p><br/><br/><p><span class="carac">Confiance</span> : '.$carac[6] ; ?></p>
                     </div>
                     <div class="row">
                        <input class="btn btn-info text-light" type="button" onclick="window.location.href='index.php?uc=praticiens&action=formulairepraticien';" value="Retour">
