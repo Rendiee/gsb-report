758b4e9e5fa8715a9ec4bc63d113a31f0abbf73a
TristanDs <d.s.tristan@outlook.fr>
2021-12-05 18:58:48 +0100

    css accueil

--- vues/v_accueil.php
+++ vues/v_accueil.php
@@ -1,6 +1,6 @@
 
-<div id="work_banner" class="banner-wrapper bg-light w-100 py-5">
-    <div class="banner-vertical-center-work container text-light d-flex justify-content-center align-items-center py-5 p-0">
+<div id="work_banner" class="banner-wrapper bg-light w-100 py-12">
+    <div class="banner-vertical-center-work container text-light d-flex justify-content-center align-items-center py-12 p-0">
         <div class="banner-content col-lg-8 col-12 m-lg-auto text-center">
             <h1 class="banner-heading h2 display-3 pb-5 semi-bold-600"><u>Projet GSB</u></h1>
             <p class="banner-body pb-2 light-300"><strong>
@@ -26,8 +26,8 @@
 
                     ?>
 
-                    <a href="index.php?uc=connexion&action=connexion">
-                    <button type="submit" class="btn rounded-pill btn-outline-light px-4 me-4 light-300">Rapport de visite</button>
+                    <a href="index.php?uc=connexion&action=profil">
+                    <button type="submit" class="btn rounded-pill btn-outline-light px-4 me-4 light-300">Profil</button>
                     </a>
 
                     <?php
