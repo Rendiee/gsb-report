bebc159f5cba6419db0d0315fdc4513f32b22bd7
Randy <randy.dur6@gmail.com>
2022-02-22 16:09:23 +0100

    changement page erreur

--- vues/v_interdit.php
+++ vues/v_interdit.php
@@ -1,10 +1,10 @@
 <section class="bg-light py-5">
-    <div class="feature-work container my-4">
-        <div class="row d-flex d-flex align-items-center">
-            <div class="col-lg-5">
-                <h1 class="feature-work-heading h2 py-3 semi-bold-600">Accès interdit !</h1>
-                <p class="feature-work-body text-muted light-300">Veuillez-vous connecter pour avoir accès à cette page.</p>
-                <p><a href="index.php?uc=connexion&action=connexion">Se connecter</a></p>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-md-auto background-error">
+                <h1 class="h2 semi-bold-600 title-error">Accès interdit !</h1>
+                <p class="text-muted light-300 text-error">Veuillez-vous connecter pour avoir accès à cette page.</p>
+                <p><a class="link-error" href="index.php?uc=connexion&action=connexion">Se connecter</a></p>
             </div>
         </div>
     </div>
