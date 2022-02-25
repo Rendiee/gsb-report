0f22677cd1ce211f505e4a283aaadc4a47867e2a
Randy <randy.dur6@gmail.com>
2022-02-25 12:44:39 +0100

    change css formulaire page

--- vues/v_formulairemedoc.php
+++ vues/v_formulairemedoc.php
@@ -1,17 +1,19 @@
 <section class="bg-light">
     <div class="container">
         <div class="row align-items-center justify-content-between">
-            <div class="col test">
+            <div class="structure-hero">
                 <h1 class="titre">Formulaire de médicament</h1>
                 <p class="text">
                     Formulaire permettant d'afficher toutes les informations
                     à propos d'un médicament en particulier.
                 </p>
-                <img class="img-fluid" src="assets/img/medoc.jpeg">
+            </div>
+            <div class="col test">
+                <img class="img-fluid size-img-page" src="assets/img/medoc.jpeg">
             </div>
             <div class="col test">
                 <form action="index.php?uc=medicaments&action=affichermedoc" method="post" class="formulaire-recherche">
-                    <label for="listemedoc">Liste des médicaments :</label>
+                    <label class="titre-formulaire" for="listemedoc">Médicaments disponible :</label>
                     <select name="medicament" id="listechoix">
                         <option value="default">- Choisissez un médicament -</option>
                         <?php                            
--- vues/v_formulairepraticien.php
+++ vues/v_formulairepraticien.php
@@ -1,17 +1,19 @@
 <section class="bg-light">
     <div class="container">
         <div class="row align-items-center justify-content-between">
-            <div class="col test">
+            <div class="structure-hero">
                 <h1 class="titre">Formulaire de praticien</h1>
                 <p class="text">
                     Formulaire permettant d'afficher les informations
                     à propos d'un praticien en particulier.
                 </p>
-                <img class="img-fluid" src="assets/img/medecin.jpg">
+            </div>
+            <div class="col test">
+                <img class="img-fluid size-img-page" src="assets/img/medecin.jpg">
             </div>
             <div class="col test">
                 <form action="index.php?uc=praticiens&action=afficherpraticien" method="post" class="formulaire-recherche">
-                    <label for="listepraticien">Liste des praticiens :</label>
+                    <label class="titre-formulaire" for="listepraticien">Praticiens disponible :</label>
                     <select name="praticien" id="listechoix">
                         <option value="default">- Choisissez un praticien -</option>
                         <?php                               
