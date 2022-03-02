88a3a6b61526e75b80d529d421a9514f40d875a4
Randy DURELLE <randy.dur6@gmail.com>
2022-03-02 12:25:29 +0100

    time unix

--- vues/v_formulairerapportnonvalide.php
+++ vues/v_formulairerapportnonvalide.php
@@ -19,13 +19,13 @@
                             <select required name="nonValide" id="listechoix">
                                 <option class="form-control text-center" value>- Rapport non valide -</option>
                                 <?php
-                                echo $key['RAP_DATEVISITE'];          
                                     foreach($info as $key){
-                                        $dateFormat = DateTime::createFromFormat("d-m-Y", $key['RAP_DATEVISITE']);
-                                        echo '<option class="form-control" value="'.$key['RAP_NUM'].'">N°'.$key['RAP_NUM'].' : '.$dateFormat.'</option>';
+                                        $phpdate = strtotime($key['RAP_DATEVISITE']);
+
+                                        //$dateFormat = DateTime::createFromFormat("d-m-Y", $key['RAP_DATEVISITE']);
+                                        echo '<option class="form-control" value="'.$key['RAP_NUM'].'">N°'.$key['RAP_NUM'].' : '.$phpdate.'</option>';
                                     }
                                     
-                                    echo $key['RAP_DATEVISITE'];
                                 ?>
                             </select>
                             <input class="btn btn-info text-light valider w-auto" type="submit" value="Modifier le rapport">
