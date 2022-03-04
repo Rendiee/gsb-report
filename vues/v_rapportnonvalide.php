fa31c1bf393de04a9e5fd3224f4c7586f23d588f
TristanDs <d.s.tristan@outlook.fr>
2022-03-04 15:47:10 +0100

    css rediger formulaire

--- vues/v_rapportnonvalide.php
+++ vues/v_rapportnonvalide.php
@@ -3,77 +3,74 @@
         <div class="structure-hero pt-lg-5 pt-4">
             <h1 class="titre text-center">Modification du rapport n°<?php echo $nonValide[1]; ?></h1>
             <p class="text text-center">
-                Formulaire permettant de rédiger un rapport de visite.
+                Formulaire permettant de modifier un rapport de visite.
             </p>
         </div>
         <div class="row align-items-center justify-content-center">
             <div class="test col-12 col-sm-9 col-lg-8 col-xl-7 py-3">
-                    <form action="index.php?uc=rapportdevisite&action=redigerrapport" method="post" class="rediger formulaire mb-0 mt-0 d-flex align-items-center flex-column">
-                        
-                        <label class="title-formulaire">Rapport de visite</label><br/>
-                        <div class="d-flex h-100">
-                            <div class="w-50 h-100 d-flex flex-column justify-content-between">
-                                <div>
-                                    <label for="nbrapport">Numéro du rapport :</label>
-                                    <input type="text" disabled name="nbrapport" class="w-25 bg-white border-0 m-0" value="<?php echo $nonValide[1];?>">
-                                </div>
-                                <div>
-                                    <label for="matricule">Matricule du collaborateur :</label>
-                                    <input type="text" disabled name="medicament" class="w-25 bg-white border-0 m-0" value="<?php echo $nonValide[0];?>">
-                                </div>
-                                <div class="d-flex flex-column flex-xxl-row">
-                                    <label for="praticien">Praticien concerné :&nbsp</label>
-                                    <input type="text" disabled class="m-0 text-truncate w-50 form-control py-0 text-rapport" name="medicament" id="nbrapport" value="<?php echo $nomPraticien[0].' - '.$nomPraticien[1].' '.$nomPraticien[2];?>">
-                                </div>
-                                <div class="d-flex flex-column flex-xxl-row">
-                                    <label for="datevisite">Date de visite :&nbsp</label>
-                                    <input type="date" class="m-0 form-control py-0 d-flex w-50 text-rapport" name="datevisite" id="datevisite" value="<?php echo $nonValide[2];?>">
-                                </div>
-                                <div>
-                                    <label for="bilan">Bilan du rapport :</label>
-                                    <textarea class="form-control m-0" name="bilanrapport" id="bilanrapport"><?php echo $nonValide[3];?></textarea>
-                                </div>
+                <form action="index.php?uc=rapportdevisite&action=redigerrapport" method="post" class="rediger formulaire mb-0 mt-0 d-flex align-items-center flex-column">                        
+                    <label class="title-formulaire">Rapport de visite</label><br/>
+                    <div class="d-flex h-100">
+                        <div class="w-50 h-100 d-flex flex-column justify-content-between">
+                            <div>
+                                <label for="nbrapport">Numéro du rapport :</label>
+                                <input type="text" disabled name="nbrapport" class="w-25 bg-white border-0 m-0" value="<?php echo $nonValide[1];?>">
                             </div>
-                            <div class="vr m-3"></div>
-                            <div class="w-50 h-100 d-flex flex-column justify-content-between">
-                                <div class="d-flex flex-column flex-xxl-row">
-                                    <label for="datesaisit">Date de saisie :&nbsp</label>
-                                    <input type="date" disabled class="form-control m-0 py-0 w-50 text-rapport" name="datesaisit" value="<?php echo $nonValide[4];?>">
-                                </div>
-                                <div class="d-flex">
-                                    <label for="motif">Motif :&nbsp</label>
-                                    <input type="text" disabled class="m-0 form-control py-0 d-flex w-50" name="datesaisit" id="datesaisit" value="<?php echo $nomMotif[0].' - '.$nomMotif[1];?>">
-                                </div>
-                                <div  class="d-flex flex-column">
-                                    <label for="medicamentproposer">Médicament présenté :</label>
-                                    <select name="motif" id="listemotif" class="form-select">                        
-                                        <option class="form-control" value="<?php echo $nonValide[7];?>" ><?php echo $nonValide[7];?></option>
-                                            <?php
-                                                foreach($medoc as $key){
-                                                    echo '<option class="form-control" value="'.$key['MED_DEPOTLEGAL'].'">'.$key['MED_DEPOTLEGAL'].' - '.$key['MED_NOMCOMMERCIAL'].'</option>';
-                                                }                                
-                                            ?>
-                                    </select>
-                                </div>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="echantillon">
-                                        <label for="echantillon" class="form-check-label">Échantillon distribué</label>                            
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="saisitdefinitive" id="saisitdefinitive">
-                                        <label for="saisitdefinitive" class="form-check-label ">Saisie définitive</label><br/>                            
-                                    </div>  
-                                </div>
-                            </div>                            
+                            <div>
+                                <label for="matricule">Matricule du collaborateur :</label>
+                                <input type="text" disabled name="medicament" class="w-25 bg-white border-0 m-0" value="<?php echo $nonValide[0];?>">
+                            </div>
+                            <div class="d-flex flex-column flex-xxl-row">
+                                <label for="praticien">Praticien concerné :&nbsp</label>
+                                <input type="text" disabled class="m-0 text-truncate w-50 form-control py-0 text-rapport" name="medicament" id="nbrapport" value="<?php echo $nomPraticien[0].' - '.$nomPraticien[1].' '.$nomPraticien[2];?>">
+                            </div>
+                            <div class="d-flex flex-column flex-xxl-row">
+                                <label for="datevisite">Date de visite :&nbsp</label>
+                                <input type="date" class="m-0 form-control py-0 d-flex w-50 text-rapport" name="datevisite" id="datevisite" value="<?php echo $nonValide[2];?>">
+                            </div>
+                            <div>
+                                <label for="bilan">Bilan du rapport :</label>
+                                <textarea class="form-control m-0" name="bilanrapport" id="bilanrapport"><?php echo $nonValide[3];?></textarea>
+                            </div>
                         </div>
-                        <div class="d-flex w-100 justify-content-center pt-3">
-                            <input class="btn btn-info text-light valider col-xl-3 col-6 col-sm-5 col-md-4 m-0 me-1" type="submit" value="Valider le rapport">
-                            <input class="btn btn-info text-light valider col-xl-3 col-6 col-sm-5 col-md-4 m-0 ms-1" type="button" onclick="history.go(-1)" value="Retour">
-                        </div>
-                    </form>
-                    
-                </div>                    
+                        <div class="vr m-3"></div>
+                        <div class="w-50 h-100 d-flex flex-column justify-content-between">
+                            <div class="d-flex flex-column flex-xxl-row">
+                                <label for="datesaisit">Date de saisie :&nbsp</label>
+                                <input type="date" disabled class="form-control m-0 py-0 w-50 text-rapport" name="datesaisit" value="<?php echo $nonValide[4];?>">
+                            </div>
+                            <div class="d-flex">
+                                <label for="motif">Motif :&nbsp</label>
+                                <input type="text" disabled class="m-0 form-control py-0 d-flex w-50" name="datesaisit" id="datesaisit" value="<?php echo $nomMotif[0].' - '.$nomMotif[1];?>">
+                            </div>
+                            <div  class="d-flex flex-column">
+                                <label for="medicamentproposer">Médicament présenté :</label>
+                                <select name="motif" id="listemotif" class="form-select">                        
+                                    <option class="form-control" value="<?php echo $nonValide[7];?>" ><?php echo $nonValide[7];?></option>
+                                        <?php
+                                        foreach($medoc as $key){
+                                             echo '<option class="form-control" value="'.$key['MED_DEPOTLEGAL'].'">'.$key['MED_DEPOTLEGAL'].' - '.$key['MED_NOMCOMMERCIAL'].'</option>';
+                                        }                                
+                                        ?>
+                                </select>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="echantillon">
+                                    <label for="echantillon" class="form-check-label">Échantillon distribué</label>                            
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="saisitdefinitive" id="saisitdefinitive">
+                                    <label for="saisitdefinitive" class="form-check-label ">Saisie définitive</label><br/>                            
+                                </div>  
+                            </div>
+                        </div>                            
+                    </div>
+                    <div class="d-flex w-100 justify-content-center pt-3">
+                        <input class="btn btn-info text-light valider col-xl-3 col-6 col-sm-5 col-md-4 m-0 me-1" type="submit" value="Valider le rapport">
+                        <input class="btn btn-info text-light valider col-xl-3 col-6 col-sm-5 col-md-4 m-0 ms-1" type="button" onclick="history.go(-1)" value="Retour">
+                    </div>
+                </form>
             </div>
         </div>
     </div>
--- vues/v_redigerrapport.php
+++ vues/v_redigerrapport.php
@@ -1,100 +1,102 @@
 <section class="bg-light">
     <div class="container">
         <div class="structure-hero pt-lg-5 pt-4">
-            <h1 class="titre text-center">Formulaire de rapport de visite</h1>
+            <h1 class="titre text-center">Saisie du rapport N°<?php echo $num;?></h1>
             <p class="text text-center">
                 Formulaire permettant de rédiger un rapport de visite.
             </p>
         </div>
         <div class="row align-items-center justify-content-center">
-            <div class="test col-12 col-sm-8 col-lg-6 col-xl-5 col-xxl-4 py-lg-5 py-3">
-                <?php if (isset($succes)){echo $succes;} ?>
-                <form action="index.php?uc=rapportdevisite&action=redigerrapport" method="post" class="formulaire mb-0">
-                    <p style="color:grey; margin-top:-10px"><span style="color:red">* </span>Champs obligatoires</p>
-                    <label class="space title-formulaire">Rapport de visite</label>
-                    <div class="form-group space">
-                        <label for="nbrapport">Numéro du rapport :</label>
-                        <input class="w-25 bg-white border-0" type="text" name="nbrapport" value="<?php echo $num;?>" disabled>
-                    </div>
-                    <div class="form-group space">
-                        <label for="matricule">Matricule du collaborateur :</label>
-                        <input class="w-25 bg-white border-0" type="text" name="matricule" value="<?php echo $_SESSION['matricule'];?>" disabled>
-                    </div>
-                    <div class="form-group space">
-                        <label for="praticien">Praticien concerné <span style="color:red">*</span> :</label>
-                        <select required name="praticien" id="listemotif">
-                            <option value="default">- Choisissez un praticien -</option>
-                            <?php
-                                foreach($prat as $key){
-                                    echo '<option value="'.$key['PRA_NUM'].'">'.$key['PRA_NUM'].' - '.$key['PRA_NOM'].' '.$key['PRA_PRENOM'].'</option>';
-                                }                                
-                            ?>
-                        </select>
-                    </div>
-                    <div class="form-group space">
-                        <label for="datevisite">Date de visite : </label>
-                        <input type="date" name="datevisite" id="datevisite">
-                    </div>
-                    <div class="form-group space">
-                        <label for="bilan">Bilan du rapport :</label>
-                        <textarea name="bilanrapport" id="bilanrapport"></textarea>
-                    </div>
-                    <div class="form-group space">
-                        <label for="datesaisit">Date de saisie du rapport <span style="color:red">*</span> : </label>
-                        <input type="date" name="datesaisit" id="datesaisit">
-                    </div>
-                    <div class="form-group space">
-                        <label for="motif">Motif <span style="color:red">*</span> :</label>
-                        <select name="motif" id="listemotif">
-                            <option value="default" >- Choisissez un motif -</option>
-                            <?php
-                                foreach($motif as $key){
-                                    echo '<option value="'.$key['MOT_ID'].'">'.$key['MOT_ID'].' - '.$key['MOT_LIBELLE'].'</option>';
-                                }                                    
-                            ?>
-                        </select>
-                    </div>
-                    <div class="form-group space">
-                        <label for="medicamentproposer">1er médicament présenté :</label>
-                        <select name="medicamentproposer" id="listemotif">
-                            <option value="default">- Choisissez un médicament -</option>
-                            <?php
-                                foreach($medoc as $key){
-                                    echo '<option value="'.$key['MED_DEPOTLEGAL'].'">'.$key['MED_DEPOTLEGAL'].' - '.$key['MED_NOMCOMMERCIAL'].'</option>';
-                                }                                
-                            ?>
-                        </select>
-                    </div>
-                    <div class="form-group space">
-                        <label for="medicamentproposer">2ème médicament présenté :</label>
-                        <select name="medicamentproposer" id="listemotif">
-                            <option value="default">- Choisissez un médicament -</option>
-                            <?php
-                                foreach($medoc as $key){
-                                    echo '<option value="'.$key['MED_DEPOTLEGAL'].'">'.$key['MED_DEPOTLEGAL'].' - '.$key['MED_NOMCOMMERCIAL'].'</option>';
-                                }                                
-                            ?>
-                        </select>
-                    </div>
-                    <div class="container">
-                        <div class="row justify-content-between">
-                            <div class="col">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="echantillon" name="echantillon" id="echantillon">
-                                    <label class="form-check-label" for="echantillon">Échantillon distribué</label>
-                                </div>
+            <div class="test col-12 col-sm-9 col-lg-8 col-xl-7 py-3">
+                <form action="index.php?uc=rapportdevisite&action=redigerrapport" method="post" class="rediger formulaire mb-0 mt-0 d-flex align-items-center flex-column">
+                    <p class="w-100 m-0 text-black-50"><span style="color:red">* </span>Champs obligatoires</p>
+                    <label class="title-formulaire pb-3">Rapport de visite</label>
+                    <div class="d-flex h-100">
+                        <div class="w-50 h-100 d-flex flex-column justify-content-between">
+                            <div>
+                                <label for="nbrapport">Numéro du rapport :</label>
+                                <input class="w-25 bg-white border-0 m-0" type="text" name="nbrapport" value="<?php echo $num;?>" disabled>
                             </div>
-                            <div class="col">
+                            <div>
+                                <label for="matricule">Matricule du collaborateur :</label>
+                                <input class="w-25 bg-white border-0 m-0" type="text" name="matricule" value="<?php echo $_SESSION['matricule'];?>" disabled>
+                            </div>
+                            <div>
+                                <label for="praticien">Praticien concerné <span style="color:red">*</span> :</label>
+                                <select required name="praticien" id="listemotif" class="form-select m-0">
+                                    <option value="default">- Choisissez un praticien -</option>
+                                    <?php
+                                        foreach($prat as $key){
+                                            echo '<option value="'.$key['PRA_NUM'].'">'.$key['PRA_NUM'].' - '.$key['PRA_NOM'].' '.$key['PRA_PRENOM'].'</option>';
+                                        }                                
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="d-flex flex-column flex-xxl-row">
+                                <label for="datevisite">Date de visite :&nbsp</label>
+                                <input type="date" class="form-control m-0 py-0 w-50 text-rapport" name="datevisite" id="datevisite">
+                            </div>
+                            <div>
+                                <label for="bilan">Bilan du rapport :</label>
+                                <textarea name="bilanrapport" id="bilanrapport" class="form-control m-0"></textarea>
+                            </div>
+                            </div>
+                        <div class="vr m-3"></div>
+                        <div class="w-50 h-100 d-flex flex-column justify-content-between">
+                            <div class="d-flex flex-column flex-xxl-row">
+                                <label for="datesaisit">Date de saisie <span style="color:red">*</span> :&nbsp</label>
+                                <input type="date" class="form-control m-0 py-0 w-50 text-rapport" name="datesaisit" id="datesaisit">
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <label for="motif">Motif <span style="color:red">*</span> :&nbsp</label>
+                                <select name="motif" id="listemotif" class="form-select w-75 m-0">
+                                    <option value="default" >- Choisissez un motif -</option>
+                                    <?php
+                                        foreach($motif as $key){
+                                            echo '<option value="'.$key['MOT_ID'].'">'.$key['MOT_ID'].' - '.$key['MOT_LIBELLE'].'</option>';
+                                        }                                    
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="d-flex flex-column">
+                                <label for="medicamentproposer">1er médicament présenté :</label>
+                                <select name="medicamentproposer" id="listemotif" class="form-select m-0">
+                                    <option value="default">- Choisissez un médicament -</option>
+                                    <?php
+                                        foreach($medoc as $key){
+                                            echo '<option value="'.$key['MED_DEPOTLEGAL'].'">'.$key['MED_DEPOTLEGAL'].' - '.$key['MED_NOMCOMMERCIAL'].'</option>';
+                                        }                                
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="d-flex flex-column">
+                                <label for="medicamentproposer">2ème médicament présenté :</label>
+                                <select name="medicamentproposer" id="listemotif" class="form-select m-0">
+                                    <option value="default">- Choisissez un médicament -</option>
+                                    <?php
+                                        foreach($medoc as $key){
+                                            echo '<option value="'.$key['MED_DEPOTLEGAL'].'">'.$key['MED_DEPOTLEGAL'].' - '.$key['MED_NOMCOMMERCIAL'].'</option>';
+                                        }                                
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="saisitdefinitive" name="saisitdefinitive" id="saisitdefinitive">
-                                    <label class="form-check-label" for="saisitdefinitive">Saisie définitive</label>
+                                    <input class="form-check-input" type="checkbox" name="echantillon">
+                                    <label for="echantillon" class="form-check-label">Échantillon distribué</label>                            
                                 </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="saisitdefinitive" id="saisitdefinitive">
+                                    <label for="saisitdefinitive" class="form-check-label ">Saisie définitive</label><br/>                            
+                                </div>  
                             </div>
                         </div>
                     </div>
-                    <button type="submit" name="valider" id="valider" class="btn btn-info text-light valider mt-3">Valider</button>
+                    <div class="d-flex w-100 justify-content-center pt-3">
+                        <input class="btn btn-info text-light valider col-xl-3 col-6 col-sm-5 col-md-4 m-0 me-1" type="submit" value="Valider le rapport">
+                        <input class="btn btn-info text-light valider col-xl-3 col-6 col-sm-5 col-md-4 m-0 ms-1" type="button" onclick="history.go(-1)" value="Retour">
+                    </div>
                 </form>
-                <input class="btn btn-info text-light valider my-5" type="button" onclick="history.go(-1)" value="Retour">
             </div>
         </div>
     </div>
