c85b4c3659de067739e26e6df91817debf028b05
Tristan <d.s.tristan@outlook.fr>
2022-03-29 17:15:35 +0200

    avancer js de rediger rapport -> js echantillon en cours

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,26 +1,15 @@
-// $("#listemotif").change(function () {
-//         if ($(this).val() == 9) {
-//             $("#text-motif-autre").append("<input id='motif-autre' class='w-25 bg-white border-0 m-0' type='text' name='motif-autre'>");
-//             $("#txtOther").attr("disabled", "disabled");
-//             alert('test');
-//         } else {
-//             $("#text-motif-autre").remove("#motif-autre");
-//             $("#txtOther").removeAttr("disabled");
-//             alert('test2');
-//         }
-//     });
 function addMotifAutre(mot) {
 	if ($(mot).val() == 9) {
-		$("#divMotif").after(
-			'<textarea required name="motif-autre" id="motif-autre" placeholder="Veuillez saisir le motif autre" class="form-control m-0"></textarea>'
+		$("#divmotifautre").append(
+			'<textarea required name="motif-autre" id="motif-autre" placeholder="Veuillez saisir le motif autre" class="form-control m-0 mt-2"></textarea>'
 		);
+		$("#motif-autre").focus();
 	} else {
 		$("#motif-autre").remove();
 	}
 }
-
-function addMedicament(mot) {
-	if ($(mot).val() != "default") {
+function addMedicament(med) {
+	if ($(med).val() != "default") {
 		$("#medoc-autre").remove();
 		$("#medoc").after(
 			$('<div class="d-flex flex-column" id="medoc-autre">').append(
@@ -32,8 +21,18 @@ function addMedicament(mot) {
 				'<option value="default">- Choisissez un médicament -</option>'
 			)
 		);
-		$.ajax({});
+		$( ".listemedoc" ).clone().appendTo( "#medicamentproposer2" );
+		var val = $(med).val();
+		$("#medicamentproposer2>.listemedoc[value='"+val+"']").remove();
 	} else {
 		$("#medoc-autre").remove();
+	}
+}
+
+function addEchantillon(ech){
+	if(ech.checked){		
+		$('#redigerEtEchantillon').after('<div id="addechantillon">eoh</div>');
+	}else{
+		$('#addechantillon').remove();
 	}
 }
--- vues/v_redigerrapport.php
+++ vues/v_redigerrapport.php
@@ -12,90 +12,83 @@
                 <form action="index.php?uc=rapportdevisite&action=redigerrapport" method="post" class="rediger formulaire mb-0 mt-0 d-flex align-items-center flex-column">
                     <p class="w-100 m-0 text-black-50"><span style="color:red">* </span>Champs obligatoires</p>
                     <div class="title-formulaire pb-3">Rapport de visite</div>
-                    <div class="d-flex h-100">
-                        <div class="w-50 h-100 d-flex flex-column justify-content-between">
-                            <div>
-                                <label for="nbrapport">Numéro du rapport :</label>
-                                <input class="w-25 bg-white border-0 m-0" type="text" name="nbrapport" id="nbrapport" value="<?php echo $num;?>" disabled>
-                            </div>
-                            <div>
-                                <label for="matricule">Matricule du collaborateur :</label>
-                                <input class="w-25 bg-white border-0 m-0" type="text" name="matricule" id="matricule" value="<?php echo $_SESSION['matricule'];?>" disabled>
-                            </div>
-                            <div>
-                                <label for="praticien">Praticien concerné <span style="color:red">*</span> :</label>
-                                <select required name="praticien" id="praticien" class="form-select m-0">
-                                    <option value>- Choisissez un praticien -</option>
-                                    <?php
-                                        foreach($prat as $key){
-                                            echo '<option value="'.$key['PRA_NUM'].'">'.$key['PRA_NUM'].' - '.$key['PRA_NOM'].' '.$key['PRA_PRENOM'].'</option>';
-                                        }                                
-                                    ?>
-                                </select>
-                            </div>
-                            <div class="d-flex flex-column flex-xxl-row">
-                                <label for="datevisite">Date de visite <span style="color:red">*</span> :&nbsp</label>
-                                <input required type="date" class="form-control m-0 py-0 w-50 text-rapport" name="datevisite" id="datevisite">
-                            </div>
-                            <div>
-                                <label for="bilanrapport">Bilan du rapport <span style="color:red">*</span> :</label>
-                                <textarea required name="bilanrapport" id="bilanrapport" class="form-control m-0"></textarea>
-                            </div>
-                            </div>
-                        <div class="vr m-3"></div>
-                        <div class="w-50 h-100 d-flex flex-column justify-content-between">
-                            <div class="d-flex flex-column flex-xxl-row">
-                                <label for="datesaisit">Date de saisie <span style="color:red">*</span> :&nbsp</label>
-                                <input required type="date" class="form-control m-0 py-0 w-50 text-rapport" name="datesaisit" id="datesaisit">
-                            </div>
-                            <div class="d-flex justify-content-between align-items-center" id="divMotif">
-                                <label for="listemotif">Motif <span style="color:red">*</span> :&nbsp</label>
-                                <select onChange="addMotifAutre(this);" required name="listemotif" id="listemotif" class="form-select w-75 m-0">
-                                    <option value >- Choisissez un motif -</option>
-                                    <?php
-                                        foreach($motif as $key){
-                                            echo '<option value="'.$key['MOT_ID'].'">'.$key['MOT_ID'].' - '.$key['MOT_LIBELLE'].'</option>';
-                                        }                         
-                                    ?>
-                                    <option value="9">9 - Autre</option>
-                                </select>
-                            </div>
-                            <div id="medoc"class="d-flex flex-column">
-                                <label for="medicamentproposer1">1er médicament présenté :</label>
-                                <select onChange="addMedicament(this);" name="medicamentproposer1" id="medicamentproposer1" class="form-select m-0">
-                                    <option value="default">- Choisissez un médicament -</option>
-                                    <?php
-                                        foreach($medoc as $key){
-                                            echo '<option value="'.$key['MED_DEPOTLEGAL'].'">'.$key['MED_DEPOTLEGAL'].' - '.$key['MED_NOMCOMMERCIAL'].'</option>';
-                                        }                                
-                                    ?>
-                                </select>
-                            </div>
-                            <!-- <div class="d-flex flex-column">
-                                <label for="medicamentproposer2">2ème médicament présenté :</label>
-                                <select name="medicamentproposer2" id="medicamentproposer2" class="form-select m-0">
-                                    <option value="default">- Choisissez un médicament -</option>
-                                    <?php
-                                        // foreach($medoc as $key){
-                                        //     echo '<option value="'.$key['MED_DEPOTLEGAL'].'">'.$key['MED_DEPOTLEGAL'].' - '.$key['MED_NOMCOMMERCIAL'].'</option>';
-                                        // }                                
-                                    ?>
-                                </select>
-                            </div> -->
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="form-check">
-                                    <label for="echantillon" class="form-check-label">Échantillon distribué</label>
-                                    <input class="form-check-input" type="checkbox" name="echantillon" id="echantillon">
+                    <div class="d-flex flex-column mesRapports">
+                        <div class="d-flex rediger" id="redigerEtEchantillon">
+                            <div class="w-50 h-100 d-flex flex-column justify-content-between">
+                                <div>
+                                    <label for="nbrapport">Numéro du rapport :</label>
+                                    <input class="w-25 bg-white border-0 m-0" type="text" name="nbrapport" id="nbrapport" value="<?php echo $num;?>" disabled>
                                 </div>
-                                <div class="form-check">
-                                    <label for="saisitdefinitive" class="form-check-label ">Saisie définitive</label>
-                                    <input class="form-check-input" type="checkbox" name="saisitdefinitive" id="saisitdefinitive"><br/>
-                                </div>  
+                                <div>
+                                    <label for="matricule">Matricule du collaborateur :</label>
+                                    <input class="w-25 bg-white border-0 m-0" type="text" name="matricule" id="matricule" value="<?php echo $_SESSION['matricule'];?>" disabled>
+                                </div>
+                                <div>
+                                    <label for="praticien">Praticien concerné <span style="color:red">*</span> :</label>
+                                    <select required name="praticien" id="praticien" class="form-select m-0">
+                                        <option value>- Choisissez un praticien -</option>
+                                        <?php
+                                            foreach($prat as $key){
+                                                echo '<option value="'.$key['PRA_NUM'].'">'.$key['PRA_NUM'].' - '.$key['PRA_NOM'].' '.$key['PRA_PRENOM'].'</option>';
+                                            }                                
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="d-flex flex-column flex-xxl-row">
+                                    <label for="datesaisit">Date de saisie <span style="color:red">*</span> :&nbsp</label>
+                                    <input required type="date" class="form-control m-0 py-0 w-50 text-rapport" name="datesaisit" id="datesaisit">
+                                </div>
+                                <div>
+                                    <label for="bilanrapport">Bilan du rapport <span style="color:red">*</span> :</label>
+                                    <textarea required name="bilanrapport" id="bilanrapport" class="form-control m-0"></textarea>
+                                </div>
+                                </div>
+                            <div class="vr m-3 h-100"></div>
+                            <div class="w-50 h-100 d-flex flex-column justify-content-between">
+                                <div class="d-flex flex-column flex-xxl-row">
+                                    <label for="datevisite">Date de visite <span style="color:red">*</span> :&nbsp</label>
+                                    <input required type="date" class="form-control m-0 py-0 w-50 text-rapport" name="datevisite" id="datevisite">
+                                </div>
+                                <div id="divmotifautre">
+                                    <div class="d-flex justify-content-between align-items-center" id="divMotif">
+                                        <label for="listemotif">Motif <span style="color:red">*</span> :&nbsp</label>
+                                        <select onChange="addMotifAutre(this);" required name="listemotif" id="listemotif" class="form-select w-75 m-0">
+                                            <option value >- Choisissez un motif -</option>
+                                            <?php
+                                                foreach($motif as $key){
+                                                    echo '<option value="'.$key['MOT_ID'].'">'.$key['MOT_ID'].' - '.$key['MOT_LIBELLE'].'</option>';
+                                                }                         
+                                            ?>
+                                            <option value="9">9 - Autre</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div id="medoc"class="d-flex flex-column">
+                                    <label for="medicamentproposer1">1er médicament présenté :</label>
+                                    <select onChange="addMedicament(this);" name="medicamentproposer1" id="medicamentproposer1" class="form-select m-0">
+                                        <option value="default">- Choisissez un médicament -</option>
+                                        <?php
+                                            foreach($medoc as $key){
+                                                echo '<option class="listemedoc" value="'.$key['MED_DEPOTLEGAL'].'">'.$key['MED_DEPOTLEGAL'].' - '.$key['MED_NOMCOMMERCIAL'].'</option>';
+                                            }                                
+                                        ?>
+                                    </select>
+                                </div>
+                                <div id="divCheck" class="d-flex justify-content-between align-items-center">
+                                    <div class="form-check">
+                                        <label for="checkEnchantillon" id="labelCheck" class="form-check-label">Échantillon distribué</label>
+                                        <input class="form-check-input" onchange="addEchantillon(this);" type="checkbox" name="echantillon" id="checkEnchantillon">
+                                    </div>
+                                    <div class="form-check">
+                                        <label for="saisitdefinitive" class="form-check-label ">Saisie définitive</label>
+                                        <input class="form-check-input" type="checkbox" name="saisitdefinitive" id="saisitdefinitive"><br/>
+                                    </div>  
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="d-flex w-100 justify-content-center pt-3">
-                        <input class="btn btn-info text-light valider col-xl-3 col-6 col-sm-5 col-md-4 m-0 me-1" type="submit" value="Valider le rapport" name="valider">
+                        <input class="btn btn-info text-light valider col-xl-3 col-6 col-sm-5 col-md-4 m-0 me-1" type="submit" value="Valider le rapport" name="valider" >
                         <input class="btn btn-info text-light valider col-xl-3 col-6 col-sm-5 col-md-4 m-0 ms-1" type="button" onclick="history.go(-1)" value="Retour">
                     </div>
                 </form>
